4d30c627214ebb1dc8f3b7c3306ed2afd152f048
Kean Uy <1100015314@edu.cbe.ab.ca>
2021-05-26 13:56:08 -0600

    commit mar 26

--- src/main.js
+++ src/main.js
@@ -2,6 +2,21 @@ console.log("hello world");
 
 import Phaser from "./lib/phaser.js"
 
-console.dir(Phaser);
+import Game from './scenes/Game.js'
 
+export default new Phaser.Game({
+    type: Phaser.AUTO,
+    width: 480,
+    height: 640,
+    scene: Game,
+    physics:{
+        default: 'arcade',
+        arcade: {
+            gravity:{
+                y:200
+            },
+            debug:true
+        }
+    }
+})
 
